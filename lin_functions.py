91e9b16281197575f1a3c6b136d754f944868cf2
HeavenlyRaven <36207275+HeavenlyRaven@users.noreply.github.com>
2019-10-23 20:26:19 +0300

    Added lin_functions.py

---
+++ lin_functions.py
@@ -0,0 +1,36 @@
+from .MatrixClass import MatrixDimensionError
+
+
+def determinant(M):
+
+    """ Takes a matrix and returns its determinant calculated using Gauss-Jordan elimination"""
+
+    if M.rows != M.columns:
+        raise MatrixDimensionError("You can calculate a determinant of a square matrix only")
+
+    det = 1
+    e = 1E-9
+    n = M.rows
+    for i in range(n):
+        k = i
+        for j in range(i+1, n):
+            if abs(M[j][i]) > abs(M[k][i]):
+                k = j
+        if abs(M[k][i]) < e:
+            det = 0
+            break
+
+        M[i], M[k] = M[k], M[i]
+        if i != k:
+            det *= -1
+
+        det *= M[i][i]
+        for j in range(i+1, n):
+            M[i][j] /= M[i][i]
+        for j in range(n):
+            if j != i and abs(M[j][i]) > e:
+                for k in range(i+1, n):
+                    M[j][k] -= M[i][k] * M[j][i]
+
+    return det
+
